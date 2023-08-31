7dca489c18fd6f55e736e193347da5a35d50bedc
Bernardo de Castro <69211132+Se7enzito@users.noreply.github.com>
2023-08-31 15:31:35 -0300

    Atualização do Projeto
    Versão 0.1 do projeto, apenas para console.

--- main.py
+++ main.py
@@ -1,34 +1,72 @@
-class SistemaLogin:
-    def __init__(self):
-        self.usuarios = {}
+from user import Login as system
 
-    def registrar_usuario(self, username, password):
-        if username in self.usuarios:
-            print("Nome de usuário já existe. Escolha outro nome.")
+login_system = system("database.db", "admin", "admin")
+
+fechar_jogo = "nao"
+
+user = None
+senha = None
+nickname = None
+id = None
+
+while user == None or senha == None or nickname == None:
+    while id == None:
+        print("\nFaça login ou crie sua conta\n")
+
+        user = input("Digite seu usuário: ")
+
+        if (user in login_system.consultar_user()):
+            login = input("Deseja fazer login? (Sim/Não) ")
+
+            if (login.lower() == "sim"):
+                senha = input("Digite sua senha: ")
+
+                if (login_system.consultar_senha(user) == senha):
+                    print("Logado com sucesso, bem-vindo.")
+
+                    id = login_system.consultar_id(user)
+                else:
+                    print("Senha errada.")
+                    print("Crie um novo usuário ou envie a senha correta.")
+            else:
+                print("Ok, crie uma nova conta. Se a conta com este nome de usuário não for sua crie com outro nome.")
         else:
-            self.usuarios[username] = password
-            print("Registro bem-sucedido. Faça o login para acessar sua conta.")
+            senha = input("Digite sua senha: ")
+            nickname = input("Digite um nickname: ")
 
-    def fazer_login(self, username, password):
-        if username in self.usuarios and self.usuarios[username] == password:
-            print("Login bem-sucedido. Bem-vindo,", username)
-            return True
-        else:
-            print("Credenciais inválidas. Verifique seu nome de usuário e senha.")
-            return False
+            if (nickname in login_system.consultar_nickname()):
+                print("Este nickname ja existe, utilize outro nickname.")
+            else:
+                new_conta = login_system.inserir_login(user, senha, nickname)
 
-class Usuario:
-    def __init__(self, username):
-        self.username = username
-        self.personalizacao = {}
+                print(f"Sua conta foi criada com os seguintes dados: {new_conta}")
 
-    def personalizar(self, atributo, valor):
-        self.personalizacao[atributo] = valor
-        
-sistema = SistemaLogin()
-sistema.registrar_usuario("usuario123", "senha123")
+                id = login_system.consultar_id(user)
 
-if sistema.fazer_login("usuario123", "senha123"):
-    usuario = Usuario("usuario123")
-    usuario.personalizar("forca", 8)
-    usuario.personalizar("inteligencia", 7)
+# Inteligencia: 1 a 10
+# Força: 1 a 10
+# Cabelo: Liso, Cacheado, Ondulado
+# Cor de Pele: Branca, Parda, Preto
+
+inteligencia = int(input("Qual a inteligência do seu personagem? (1 a 10) "))
+forca = int(input("Qual a força do seu personagem? (1 a 10) "))
+cabelo = str(input("Qual seu tipo de cabelo? (Liso, Cacheado, Ondulado) "))
+corpele = str(input("Qual a cor de pele? (Branca, Parda, Preto) "))
+
+login_system.setar_cabelo(id, cabelo)
+login_system.setar_corpele(id, corpele)
+login_system.setar_forca(id, forca)
+login_system.setar_corpele(id, corpele)
+
+dados = login_system.consultar_tabela()
+
+for dado in dados:
+    print(dado[0])
+    print(dado[1])
+    print(dado[2])
+    print(dado[3])
+    print(dado[4])
+    print(dado[5])
+    print(dado[6])
+    print(dado[7])
+    
---
+++ user.py
@@ -0,0 +1,171 @@
+import sqlite3 as sql
+
+class Login:
+    def __init__(self, database: str, username: str, password: str) -> None:
+        self.database = database
+        self.username = username
+        self.password = password
+        self.connection = None
+        self.cursor = None
+        self.nickname = None
+        self.forca = None
+        self.inteligencia = None
+        self.cor_de_pele = None
+        self.cabelo = None
+        
+    
+    def setar_forca(self, id: int, valor: int):
+        self.conectar()
+        self.cursor.execute("UPDATE users (forca) WHERE id = ? VALUES (?)", (id, valor))
+        self.connection.commit()
+        self.desconectar()
+
+        return valor
+    
+    def setar_inteligencia(self, id: int, valor: int):
+        self.conectar()
+        self.cursor.execute("UPDATE users (inteligencia) WHERE id = ? VALUES (?)", (id, valor))
+        self.connection.commit()
+        self.desconectar()
+
+        return valor
+    
+    
+    def setar_cabelo(self, id: int, valor: str):
+        self.conectar()
+        self.cursor.execute("UPDATE users (cabelo) WHERE id = ? VALUES (?)", (id, valor))
+        self.connection.commit()
+        self.desconectar()
+
+        return valor
+    
+    
+    def setar_corpele(self, id: int, valor: str):
+        self.conectar()
+        self.cursor.execute("UPDATE users (cor_pele) WHERE id = ? VALUES (?)", (id, valor))
+        self.connection.commit()
+        self.desconectar()
+
+        return valor
+
+
+    def login(self) -> bool:
+        return True if self.username == "admin" and "admin" == self.password else False
+
+
+    def conectar(self) -> bool:
+        if self.login():
+            self.connection = sql.connect(self.database)
+            self.cursor = self.connection.cursor()
+            return True
+
+
+    def desconectar(self) -> bool:
+        self.connection.close()
+
+
+    def inserir_login(self, user: str, senha: str, nickname: str) -> None:
+        self.conectar()
+        self.cursor.execute("INSERT INTO users (nickname, user, senha) VALUES (?, ?, ?)", (nickname, user, senha))
+        self.connection.commit()
+        self.desconectar()
+
+        return (user, senha, nickname)
+
+
+    # def inserir_kwdado(self, **valores: dict) -> dict:
+    #     self.conectar()
+    #     self.cursor.execute("INSERT INTO users (nickname, first_name, last_name, score) VALUES (?, ?, ?, ?)", (valores['nickname'], valores['first_name'], valores['last_name'], valores['score']))
+    #     self.connection.commit()
+    #     self.desconectar()
+
+    #     return valores
+
+
+    def atualizar_dado(self, id: int, **valores: dict) -> None:
+        self.conectar()
+        self.cursor.execute("UPDATE users (nickname, user, senha) WHERE id = ? VALUES (?, ?, ?)", (id, valores['nickname'], valores['user'], valores['senha']))
+        self.connection.commit()
+        self.desconectar()
+
+        return valores
+
+
+    def apagar_dado(self, id: int) -> None:
+        self.conectar()
+        backup = self.consultar_dado(id)
+        self.cursor.execute("DELETE * FROM users WHERE id = ?", (id,))
+        self.connection.commit()
+        self.desconectar()
+
+        return backup
+
+    def consultar_dado(self, id: int) -> list:
+        self.conectar()
+        consulta = self.cursor.execute("SELECT * FROM users WHERE id = ?", (id,)).fetchall()
+        self.desconectar()
+
+        return consulta
+
+    def limpar_tabela(self) -> None:
+        self.conectar()
+        self.cursor.execute("DELETE * FROM users")
+        self.connection.commit()
+        self.desconectar()
+
+
+    def consultar_tabela(self) -> None:
+        self.conectar()
+        consulta = self.cursor.execute("SELECT * FROM users").fetchall()
+        self.desconectar()
+
+        return consulta
+    
+    
+    def consultar_user(self) -> list:
+        self.conectar()
+        consulta = self.cursor.execute("SELECT user FROM users").fetchall()
+        self.desconectar()
+    
+        return [item[0] for item in consulta]
+    
+    
+    def consultar_nickname(self) -> list:
+        self.conectar()
+        consulta = self.cursor.execute("SELECT nickname FROM users").fetchall()
+        self.desconectar()
+    
+        return [item[0] for item in consulta]
+    
+    
+    def consultar_senha(self, user: str) -> None:
+        self.conectar()
+        
+        dados = self.cursor.execute("SELECT * FROM users").fetchall()
+        
+        for dado in dados:
+            if dado[2] == user:
+                consulta = dado[3]
+        
+        # consulta = self.cursor.execute("SELECT senha FROM users WHERE id = ?", (id,)).fetchall()
+        self.desconectar()
+
+        return consulta
+    
+    
+    def consultar_id(self, user: str) -> None:
+        self.conectar()
+        
+        dados = self.cursor.execute("SELECT * FROM users").fetchall()
+        
+        for dado in dados:
+            if dado[2] == user:
+                consulta = dado[0]
+        
+        self.desconectar()
+
+        return consulta
+
+
+if __name__ == "__main__":
+    pass
